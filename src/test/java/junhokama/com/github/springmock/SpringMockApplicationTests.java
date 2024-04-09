b3bdb1cef5ba62f56a7d17ee8c3f6cca533092a0
JunHokama <ejunhok@gmail.com>
2024-04-09 09:46:36 -0300

    Initial Commit

---
+++ src/test/java/junhokama/com/github/springmock/SpringMockApplicationTests.java
@@ -0,0 +1,13 @@
+package junhokama.com.github.springmock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
