fce60489089497faeab3faa7f5726cfd64091dff
JunHokama <ejunhok@gmail.com>
2024-04-09 17:46:53 -0300

    feat: add dependencies

--- src/main/java/junhokama/com/github/springmock/SpringMockApplication.java
+++ src/main/java/junhokama/com/github/springmock/SpringMockApplication.java
@@ -1,5 +1,7 @@
 package junhokama.com.github.springmock;
 
+import junhokama.com.github.springmock.model.PhotoConsumerFeign;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,8 +15,13 @@ public class SpringMockApplication implements CommandLineRunner {
 		SpringApplication.run(SpringMockApplication.class, args);
 	}
 
+	@Autowired
+	private PhotoConsumerFeign photoConsumerFeign;
+
 	@Override
 	public void run(String... args) throws Exception {
-
+		System.out.println(
+			this.photoConsumerFeign.getPhotosById(3L)
+		);
 	}
 }
